ea7ded4370f17c584eaf716a49b4110651deab4d
Yuri Mello <yuridemeello@gmail.com>
2022-03-28 16:52:47 -0300

    removing unnecessary validations

--- src/controllers/posts/UpdatePostController.js
+++ src/controllers/posts/UpdatePostController.js
@@ -2,19 +2,9 @@ import Post from '../../models/post';
 
 export default async function updatePostController(req, res) {
   try {
-    const { id } = req.params;
-
-    if (!id) {
-      return res.send({ status: 500 });
-    }
-
     const findedPost = await Post.findOneAndUpdate({ id }, req.body, {
       new: true,
     });
-
-    if (!findedPost) {
-      return res.send({ status: 500 });
-    }
 
     return res.send(findedPost);
   } catch (err) {
