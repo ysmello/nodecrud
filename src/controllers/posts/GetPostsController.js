d45c544de0fedb4a334d1744692d8f6ca416203e
Yuri Mello <yuridemeello@gmail.com>
2022-03-28 16:07:46 -0300

    add route get post by id

--- src/controllers/posts/GetPostsController.js
+++ src/controllers/posts/GetPostsController.js
@@ -1,7 +1,19 @@
 import Post from '../../models/post';
 
-export default async function GetPostsConstructor(req, res) {
+export async function getPostsController(req, res) {
   const posts = await Post.find();
 
   return res.send({ posts });
+}
+
+export async function findOnePostController(req, res) {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.send({ status: 404 });
+  }
+
+  const post = await Post.findById(req.params.id);
+
+  return res.send({ post });
 }
--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import createPostController from '../../controllers/posts/CreatePostController';
-import getPostsController from '../../controllers/posts/GetPostsController';
+import {
+  getPostsController,
+  findOnePostController,
+} from '../../controllers/posts/GetPostsController';
 
 const router = Router();
 
 router.post('/posts', createPostController);
 router.get('/posts', getPostsController);
+router.get('/posts/:id', findOnePostController);
 
 export default router;
