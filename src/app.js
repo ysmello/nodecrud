bcf467cd966763fee0047e0eddef206e10ead98e
Yuri Mello <yuridemeello@gmail.com>
2022-03-29 00:06:37 -0300

    add jwt validation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 
+import jwtMiddleware from './helpers/jwtMiddleware';
+
 import api from './routes/api';
 
 const app = express();
 const port = 8000;
 
+app.use(jwtMiddleware);
 app.use(express.json());
 app.use(
   express.urlencoded({
---
+++ src/helpers/jwtMiddleware.js
@@ -0,0 +1,21 @@
+import jwtService from 'jsonwebtoken';
+
+function jwtMiddleware(req, res, next) {
+  const jwt = req.headers['authorization'];
+  const replaceJwt = jwt.replace('Bearer ', '');
+  const secret = 'secret';
+
+  console.log(replaceJwt);
+
+  jwtService.verify(replaceJwt, secret, (err, userInfo) => {
+    if (err) {
+      res.status(403).send('token not valid');
+      return;
+    }
+
+    req.userInfo = userInfo;
+    next();
+  });
+}
+
+export default jwtMiddleware;
